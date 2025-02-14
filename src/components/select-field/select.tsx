0962cec91969ab57fe1e11c3bb44665cafd50a17
XAITYH <petrovskijpavel1@gmail.com>
2025-02-14 16:15:07 +0300

    huge upd: добавил иконки, конфиг, стили

--- src/components/select-field/select.tsx
+++ src/components/select-field/select.tsx
@@ -2,19 +2,24 @@
 
 import { useState } from "react";
 import styles from "./select.module.scss";
+import Chevron_up from "@/shared/assets/icons/feather-icon _ chevron-up.svg";
+import Chevron_down from "@/shared/assets/icons/feather-icon _ chevron-down.svg";
+import X from "@/shared/assets/icons/feather-icon _ x.svg";
 
 type SelectProps = {
   active: boolean;
   size: "s" | "l";
   initOpt: string[];
   placeholder: string;
+  sides: "one" | "both";
 };
 
 const Select = ({
   active = true,
   size = "l",
-  initOpt = ["1", "Renault", "Mercedes"],
+  initOpt = ["1", "2", "3"],
   placeholder = "Placeholder",
+  sides = "one",
 }: SelectProps) => {
   // Массив выбранных опций
   const [options, setOptions] = useState<string[]>([]);
@@ -43,6 +48,11 @@ const Select = ({
       {active ? (
         <div className={`${styles.selectField} ${styles[size]}`}>
           <div className={styles.cont}>
+            {sides === "both" ? (
+              <span>{appear ? <Chevron_up /> : <Chevron_down />}</span>
+            ) : (
+              ""
+            )}
             <div className={styles.inputField}>
               {options.length === 0 ? (
                 <div className={styles.placeholder}>{placeholder}</div>
@@ -54,24 +64,26 @@ const Select = ({
                 ))
               )}
             </div>
-            <span onClick={handleToggleDropdown}>{appear ? "⬆" : "⬇"}</span>
+            <span onClick={handleToggleDropdown}>
+              {appear ? <Chevron_up /> : <Chevron_down />}
+            </span>
           </div>
           <div
             style={appear ? { display: "inline-block" } : { display: "none" }}
             className={styles.dropdown}
           >
             {initOpt.map((item: string, idx: number) => (
-              <p
+              <div
                 key={idx}
                 onClick={() => handleAdd(item)}
                 className={
                   options.find((el) => el === item)
-                    ? `${styles.option} ${styles.choosed}`
-                    : styles.option
+                    ? `${styles.option} ${styles.choosed} ${sides === "both" ? styles.optBoth : ""}`
+                    : sides === "both" ? `${styles.optBoth} ${styles.option}` : styles.option
                 }
               >
-                {item}
-              </p>
+                { sides === "both" ? <Chevron_up /> : ""} {item}
+              </div>
             ))}
           </div>
         </div>
@@ -84,12 +96,14 @@ const Select = ({
               ) : (
                 options.map((item: string, idx: number) => (
                   <div className={styles.option} key={idx}>
-                    {item} <span>X</span>
+                    {item} <span><X /></span>
                   </div>
                 ))
               )}
             </div>
-            <span>⬇</span>
+            <span>
+              <Chevron_up />
+            </span>
           </div>
         </div>
       )}
